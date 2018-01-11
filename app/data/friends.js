268e9aff9a66b457449806dee7ae220dbe7ebec3
ElmasSayed <elmassayed@gmail.com>
2018-01-11 12:27:10 -0800

    add celebs

--- app/data/friends.js
+++ app/data/friends.js
@@ -22,6 +22,48 @@ var friendsArray = [{
         "name": "Natalie Portman",
         "photo": "http://www.imdb.com/name/nm0000204/mediaviewer/rm512132096?ref_=nm_ov_ph",
         "scores": ["5", "1", "1", "4", "2", "5", "4", "4", "5", "1"]
-    }
+    },
+    {
+        "name": "Adele",
+        "photo": "https://www.usmagazine.com/wp-content/uploads/1350327846_adele-290.jpg?w=225&h=400&crop=1",
+        "scores": ["5", "1", "5", "4", "2", "5", "4", "4", "3", "1"]
+    },
+    {
+        "name": "Adrien Brody",
+        "photo": "https://www.usmagazine.com/wp-content/uploads/1279646509_adrien-402.jpg?w=225&h=400&crop=1",
+        "scores": ["1", "1", "4", "4", "2", "2", "4", "4", "3", "1"]
+    },
+    {
+        "name": "Alexis Bledel",
+        "photo": "https://thestarsfact.com/wp-content/uploads/2017/04/Alexis-Bledel.jpg",
+        "scores": ["1", "1", "4", "4", "2", "2", "4", "4", "3", "1"]
+    },
+    {
+        "name": "50 Cent ",
+        "photo": "https://us-east.manta.joyent.com/condenast/public/gq/production/2015/06/18/gq-features-120105-50_v.jpg",
+        "scores": ["1", "1", "4", "4", "1", "2", "4", "2", "3", "1"]
+    },
+    {
+        "name": "Alicia Keys",
+        "photo": "https://24193-presscdn-pagely.netdna-ssl.com/wp-content/uploads/2017/01/alicia-keys-no-makeup-beauty-goals-entity-1320x720.png",
+        "scores": ["3", "1", "3", "4", "2", "3", "2", "4", "1", "1"]
+    },
+    {
+        "name": "Eminem",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/61u7K%2BQP4CL._AC_UL320_SR214,320_.jpg",
+        "scores": ["1", "5", "4", "4", "1", "2", "4", "4", "3", "1"]
+    },
+    {
+        "name": "Priyanka Chopra",
+        "photo": "http://s3.india.com/wp-content/uploads/2017/05/Priyanka-Chopra-3.jpg",
+        "scores": ["4", "4", "1", "4", "2", "2", "5", "4", "3", "2"]
+    },
+    {
+        "name": "Troian Bellisario",
+        "photo": "http://www.magweb.com/picts/nm0069079.jpg",
+        "scores": ["1", "1", "5", "4", "2", "2", "2", "4", "1", "2"]
+    },
+    
+
 ]
 module.exports = friendsArray;
